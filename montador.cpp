89eb414422d48ab1acfa2702540e5c2d0a96e9fd
CristovaoBG <cristovao@live.com>
2019-09-24 15:32:42 -0300

    montador le arquivo

--- montador.cpp
+++ montador.cpp
@@ -1,1 +1,30 @@
 #include <stdio.h>
+
+#define TAMANHO_MAXIMO_ARQUIVO 1024
+
+int main(int argc, char *argv[]){
+	//abre e carrega arquivo na memoria
+	if (argc != 2) {
+		printf("uso incorreto.\n");
+		return 0;
+		}
+
+	int programa[TAMANHO_MAXIMO_ARQUIVO];
+	int tamanhoArquivo = 0;
+	FILE *file = NULL;
+	file = fopen(argv[1],"r");
+	if (file == NULL){
+		printf("arquivo não encontrado\n");
+		return 0;
+	}
+
+	fscanf(file,"%d",programa);
+	while (!feof(file) && tamanhoArquivo < 1024){
+		//printf ("%d", i);
+		tamanhoArquivo++;
+		fscanf (file, "%d", programa+tamanhoArquivo); 
+	}
+	fclose(file);
+
+	return 0;
+}
